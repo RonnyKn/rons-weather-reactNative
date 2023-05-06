bf4e383d2ada6fcbe983b83026acf87c95302f6a
RonnyKn <ronnykurniawn@gmail.com>
2023-05-06 22:07:33 +0700

    adding style to app js

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from "react"
 import CurrentWeather from "./src/components/CurrentWeather"
+import { StyleSheet, View } from "react-native"
 
 const App = () => {
-  return <CurrentWeather />
+  return (
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
 
 export default App
